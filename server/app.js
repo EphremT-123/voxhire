4997976ba817268411e6bb9e2e42deb6a024d61a
ephrem <fgitariku176@gmail.com>
2026-06-02 10:13:04 +0300

    Add request logging

--- server/app.js
+++ server/app.js
@@ -12,7 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 // Import routes
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
